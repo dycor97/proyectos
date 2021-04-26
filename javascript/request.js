a4cd64da75ba2a38a2f3af7181e2273ca240c8b6
Dylan Cortez <dycor.97@gmail.com>
2021-04-26 18:32:03 -0500

    tictactoe react

--- javascript/request.js
+++ javascript/request.js
@@ -27,13 +27,61 @@ xhr.send(data);
 
 
 //Fetch GET
-fetch('https://api-to-call.com/endpoint').then(response => {
+fetch('https://api-to-call.com/endpoint')
+.then(response => {
   if(response.ok){
     return response.json();
   }
   throw new Error('Request failed!');
   }, networkError => {
     console.log(networkError.message);
-  }).then(jsonResponse => {
-    return jsonResponse;
-  });
+  })
+.then(jsonResponse => {
+  return jsonResponse;
+});
+
+  //Fetch POST
+fetch('https://api-to-call.com/endpoint', {
+  method: 'POST',
+  body: JSON.stringify({id: '200'})
+})
+.then(response => {
+  if(response.ok){
+    return response.json();
+  }
+  throw new Error('Request failed!');
+}, networkError => {console.log(networkError.message)})
+.then(jsonResponse => {
+  return jsonResponse;
+});
+
+//async await GET
+const getData = async () => {
+  try {
+    const response = await fetch('https://api-to-call.com/endpoint');
+    if(response.ok) {
+      const jsonResponse = await response.json();
+      return jsonResponse;
+    }
+    throw new Error('Request failed!');
+  } catch(error) {
+    console.log(error);
+  }
+}
+
+//async await POST
+const getData = async () => {
+  try {
+    const response = await fetch('https://api-to-call.com/endpoint', {
+      method: 'POST',
+      body: JSON.stringify({id: 200})
+    });
+    if(response.ok) {
+      const jsonResponse = await response.json();
+      return jsonResponse;
+    }
+    throw new Error('Request failed!');
+  } catch(error) {
+    console.log(error);
+  }
+};
