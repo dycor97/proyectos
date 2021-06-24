0bda8ea663abd49260adcd480bcb52b902d798b0
Dylan Cortez <dycor.97@gmail.com>
2021-06-24 15:48:24 -0500

    first hardcoded post

---
+++ laravel/blog/resources/views/posts.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>My Blog</title>
+    <link rel="stylesheet" href="./app.css">
+</head>
+<body>
+    <article>
+        <h1><a href="/post">My First Post</a></h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo corrupti modi, qui at pariatur molestiae ullam similique natus laborum. Sit incidunt facere amet reiciendis quisquam labore numquam recusandae eos maxime.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis voluptas optio provident quia illo eveniet, harum esse eos tempore. Voluptatum nesciunt aspernatur soluta facilis illum nulla, fuga ducimus est modi.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, quam. Blanditiis quibusdam pariatur, architecto eum maiores odit sit totam voluptatum earum ad. Facere quo placeat saepe labore quibusdam, facilis veniam!
+        </p>
+    </article>
+    <article>
+        <h1><a href="/post">My Second Post</a></h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo corrupti modi, qui at pariatur molestiae ullam similique natus laborum. Sit incidunt facere amet reiciendis quisquam labore numquam recusandae eos maxime.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis voluptas optio provident quia illo eveniet, harum esse eos tempore. Voluptatum nesciunt aspernatur soluta facilis illum nulla, fuga ducimus est modi.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, quam. Blanditiis quibusdam pariatur, architecto eum maiores odit sit totam voluptatum earum ad. Facere quo placeat saepe labore quibusdam, facilis veniam!
+        </p>
+    </article>
+    <article>
+        <h1><a href="/post">My Third Post</a></h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo corrupti modi, qui at pariatur molestiae ullam similique natus laborum. Sit incidunt facere amet reiciendis quisquam labore numquam recusandae eos maxime.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis voluptas optio provident quia illo eveniet, harum esse eos tempore. Voluptatum nesciunt aspernatur soluta facilis illum nulla, fuga ducimus est modi.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, quam. Blanditiis quibusdam pariatur, architecto eum maiores odit sit totam voluptatum earum ad. Facere quo placeat saepe labore quibusdam, facilis veniam!
+        </p>
+    </article>
+</body>
+</html>
